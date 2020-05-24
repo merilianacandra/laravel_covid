68014cedb6d3e881c5c894dc64789660de062592
Meriliana <cmeriliana@yahoo.com>
2020-05-24 20:43:27 +0800

    coba

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -11,10 +11,10 @@ use Illuminate\Http\Request;
 class DataController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     /**
      * Display a listing of the resource.
      *
